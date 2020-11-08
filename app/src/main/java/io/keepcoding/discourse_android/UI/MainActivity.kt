d54bfb8e0dbbc0b5960bcfd151eb956c56f58027
Sonia Sieiro <soniasieirod@gmail.com>
2020-11-08 14:01:31 +0100

    font avenir added and small refactor

--- app/src/main/java/io/keepcoding/discourse_android/UI/MainActivity.kt
+++ app/src/main/java/io/keepcoding/discourse_android/UI/MainActivity.kt
@@ -1,8 +1,10 @@
 package io.keepcoding.discourse_android.UI
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.annotation.DrawableRes
+import androidx.core.content.res.ResourcesCompat
 import com.google.android.material.tabs.TabLayout
 import io.keepcoding.discourse_android.R
 import io.keepcoding.discourse_android.UI.profile.ProfileFragment
@@ -18,13 +20,16 @@ class MainActivity : AppCompatActivity(), TopicsFragment.TopicsInteractionListen
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_tab_topics_select))
-        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_tab_search_select))
-        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_tab_profile_select))
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_tab_topics))
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_tab_search))
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_tab_profile))
         tabLayout.tabGravity = TabLayout.GRAVITY_FILL
 
         val adapter = ViewPageAdapter(this, supportFragmentManager, tabLayout.tabCount)
         viewPager.adapter = adapter
+
+        val myCustomFont : Typeface? = ResourcesCompat.getFont(this, R.font.avenir_next_bold)
+        toolbar_text.typeface = myCustomFont
 
         tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab) {
--- app/src/main/java/io/keepcoding/discourse_android/UI/topics/TopicsAdapter.kt
+++ app/src/main/java/io/keepcoding/discourse_android/UI/topics/TopicsAdapter.kt
@@ -2,9 +2,11 @@ package io.keepcoding.discourse_android.UI.topics
 
 import android.content.Context
 import android.graphics.Color
+import android.graphics.Typeface
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -39,12 +41,20 @@ class TopicsAdapter(private val context: Context, private val callbackTopicClick
     }
 
     inner class TopicHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val myCustomFontBold : Typeface? = ResourcesCompat.getFont(context, R.font.avenir_next_bold)
+        val myCustomFontRegular : Typeface? = ResourcesCompat.getFont(context, R.font.avenir_next_regular)
         var topic: TopicItem? = null
             set(value) {
                 field = value
                 itemView.tag = field
 
                 field?.let {
+                    itemView.labelTitle.typeface = myCustomFontRegular
+                    itemView.labelPosts.typeface = myCustomFontRegular
+                    itemView.labelViews.typeface = myCustomFontRegular
+                    itemView.labelReplies.typeface = myCustomFontRegular
+                    itemView.labelDate.typeface = myCustomFontRegular
+                    itemView.posterUsername.typeface = myCustomFontBold
                     itemView.labelTitle.text = it.title
                     itemView.labelPosts.text = it.posts.toString()
                     itemView.labelViews.text = it.views.toString()
