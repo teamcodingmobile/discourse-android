f2b23ae9d4701d139ff346ed5ce59d74c6fbad8d
Adrián Arcalá <aarcala10@gmail.com>
2020-11-17 03:19:55 +0100

    searchView edited

--- app/src/main/java/io/keepcoding/discourse_android/UI/search/SearchFragment.kt
+++ app/src/main/java/io/keepcoding/discourse_android/UI/search/SearchFragment.kt
@@ -2,10 +2,8 @@ package io.keepcoding.discourse_android.UI.search
 
 import android.os.Bundle
 import android.view.*
-import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import io.keepcoding.discourse_android.R
-import io.keepcoding.discourse_android.UI.login.MainActivity
 
 class SearchFragment() : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -13,26 +11,13 @@ class SearchFragment() : Fragment() {
         return inflater.inflate(R.layout.search_fragment, container, false)
 
     }
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
-        menu.clear()
-        inflater?.inflate(R.menu.menu_search, menu)
-        val searchView = SearchView((context as MainActivity).supportActionBar?.themedContext ?: context)
-        menu.findItem(R.id.action_search).apply {
-            setShowAsAction(MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW or MenuItem.SHOW_AS_ACTION_IF_ROOM)
-            actionView = searchView
-        }
-
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String): Boolean {
-                return false
-            }
-
-            override fun onQueryTextChange(newText: String): Boolean {
-                return false
-            }
-        })
-        searchView.setOnClickListener {view ->  }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
     }
 
+    private fun init() {
+
+
+    }
 }
